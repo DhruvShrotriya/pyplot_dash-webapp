6e93a60d23891ee89a75412b76ccd6f8fad9b9ee
DhruvShrotriya <94039991+DhruvShrotriya@users.noreply.github.com>
2024-02-18 22:07:39 +0530

    Add files via upload

---
+++ country_dash.py
@@ -0,0 +1,77 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+from dash.dependencies import Input, Output
+
+url = "https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv"
+df = pd.read_csv(url)
+app = dash.Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div(children=[
+    html.H1(children='World GDP Visualization'),
+    # Choropleth map
+    dcc.Graph(
+        id='world-map',
+        figure=px.choropleth(
+            df,
+            locations='CODE',
+            color='GDP (BILLIONS)',
+            hover_name='COUNTRY',
+            title='World GDP Choropleth Map',
+            color_continuous_scale='Viridis'
+        )
+    ),
+    # Dropdown for GDP range selection
+    html.Label('Select GDP Range:'),
+    dcc.Dropdown(
+        id='gdp-range-dropdown',
+        options=[
+            {'label': '0-5 Billion', 'value': '0-5'},
+            {'label': '5-10 Billion', 'value': '5-10'},
+            {'label': '10-15 Billion', 'value': '10-15'},
+            {'label': '15-20 Billion', 'value': '15-20'},
+            {'label': '20+ Billion', 'value': '20+'},
+        ],
+        value='0-5',
+        multi=False
+    ),
+
+    # Bar chart
+    dcc.Graph(
+        id='bar-chart',
+        figure=px.bar(
+            df,
+            x='COUNTRY',
+            y='GDP (BILLIONS)',
+            title='World GDP Bar Chart'
+        )
+    )
+])
+
+# Define callback to update bar chart based on selected GDP range
+@app.callback(
+    Output('bar-chart', 'figure'),
+    [Input('gdp-range-dropdown', 'value')]
+)
+def update_bar_chart(selected_range):
+    if selected_range == '20+':
+        filtered_df = df[df['GDP (BILLIONS)'] >= 20]
+    else:
+        range_values = selected_range.split('-')
+        filtered_df = df[(df['GDP (BILLIONS)'] >= int(range_values[0])) & (df['GDP (BILLIONS)'] < int(range_values[1]))]
+
+    bar_chart = px.bar(
+        filtered_df,
+        x='COUNTRY',
+        y='GDP (BILLIONS)',
+        title=f'World GDP Bar Chart - {selected_range}',
+    )
+
+    return bar_chart.update_layout(title_text=f'World GDP Bar Chart - {selected_range}')
+
+# Run the app
+if __name__ == '__main__':
+        app.run_server()
