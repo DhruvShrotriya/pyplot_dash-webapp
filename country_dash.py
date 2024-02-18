ae179b229aaf54a64b9414e42744e32fb6123ec3
DhruvShrotriya <94039991+DhruvShrotriya@users.noreply.github.com>
2024-02-18 22:19:24 +0530

    Update country_dash.py

--- country_dash.py
+++ country_dash.py
@@ -1,9 +1,6 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
-from dash.dependencies import Input, Output
 
 url = "https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv"
 df = pd.read_csv(url)
